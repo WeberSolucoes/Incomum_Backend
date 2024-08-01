4f0f878d7cf584e7dc8497d40840f38c0ab0a55d
BrunoMartins <brunomartins616@hotmail.com>
2024-08-01 12:54:28 -0300

    Teste SQLITE

--- config/settings.py
+++ config/settings.py
@@ -132,23 +132,23 @@ DATABASE_ENGINE = config('DATABASE_ENGINE')
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-#  
-# }
 DATABASES = {
     'default': {
-        'ENGINE': f'django.db.backends.{DATABASE_ENGINE}',
-        'NAME': DATABASE_NAME,
-        'USER': DATABASE_USER,
-        'PASSWORD': DATABASE_PASSWORD,
-        'HOST': DATABASE_HOST,
-        'PORT': DATABASE_PORT,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+ 
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': f'django.db.backends.{DATABASE_ENGINE}',
+#         'NAME': DATABASE_NAME,
+#         'USER': DATABASE_USER,
+#         'PASSWORD': DATABASE_PASSWORD,
+#         'HOST': DATABASE_HOST,
+#         'PORT': DATABASE_PORT,
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
