8e13ad3060d5eeea9bd97dd6d61e2b8641082eaa
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-09-25 16:35:36 -0300

    Update usuarioService.py

--- autenticacaoWeber/services/usuarioService.py
+++ autenticacaoWeber/services/usuarioService.py
@@ -1,7 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
 from typing import List
-from decouple import config
 import re
 
 from django.db.utils import IntegrityError
