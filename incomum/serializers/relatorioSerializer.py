f54c009e1993b8746c79c2c03ad5b8025452d074
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-10-08 08:31:31 -0300

    Update relatorioSerializer.py

--- incomum/serializers/relatorioSerializer.py
+++ incomum/serializers/relatorioSerializer.py
@@ -3,52 +3,14 @@ from ..models.relatorio import Relatorio
 import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-class RelatorioSerializer(serializers.ModelSerializer):
-    nome_loja = serializers.SerializerMethodField()
-    fim_data = serializers.SerializerMethodField()
-    fim_valorliquido = serializers.SerializerMethodField()
-    fim_markup = serializers.SerializerMethodField()
-    fim_valorinc = serializers.SerializerMethodField()
-    fim_valorincajustado = serializers.SerializerMethodField()
-    ven_descricao = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Relatorio
-        fields = [
-            'fim_codigo',
-            'tur_codigo',
-            'loj_codigo',
-            'fim_data',
-            'aco_codigo',
-            'fim_tipo',
-            'tur_numerovenda',
-            'fim_valorliquido',
-            'fim_markup',
-            'fim_valorinc',
-            'fim_valorincajustado',
-            'nome_loja',
-            'ven_descricao'
-        ]
-
-    def get_ven_descricao(self, obj):
-        return obj.ven_codigo.ven_descricao if obj.ven_codigo else None
-
-    def get_nome_loja(self, obj):
-        return obj.age_codigo.age_descricao if obj.loj_codigo else None
-
-
-    
-    def get_fim_data(self, obj):
-        return obj.fim_data.strftime('%d/%m/%Y') if obj.fim_data else None
-
-    def get_fim_valorliquido(self, obj):
-        return locale.currency(obj.fim_valorliquido, grouping=True, symbol=False) if obj.fim_valorliquido else None
-
-    def get_fim_markup(self, obj):
-        return locale.currency(obj.fim_markup, grouping=True, symbol=False) if obj.fim_markup else None
-
-    def get_fim_valorinc(self, obj):
-        return locale.currency(obj.fim_valorinc, grouping=True, symbol=False) if obj.fim_valorinc else None
-
-    def get_fim_valorincajustado(self, obj):
-        return locale.currency(obj.fim_valorincajustado, grouping=True, symbol=False) if obj.fim_valorincajustado else None
+class RelatorioSerializer(serializers.Serializer):
+    aco_codigo = serializers.IntegerField(required=False, allow_null=True)
+    fim_codigo = serializers.IntegerField(required=False, allow_null=True)
+    fim_data = serializers.DateField()
+    fim_markup = serializers.FloatField()
+    fim_tipo = serializers.CharField()
+    fim_valorinc = serializers.FloatField()
+    fim_valorincajustado = serializers.FloatField()
+    fim_valorliquido = serializers.FloatField()
+    tur_codigo = serializers.IntegerField()
+    tur_numerovenda = serializers.CharField()
