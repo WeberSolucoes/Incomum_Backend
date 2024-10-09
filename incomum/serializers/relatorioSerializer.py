7d0381881b548c78c4ffe822d3f4fedaf6851e0a
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-10-09 11:28:47 -0300

    Update relatorioSerializer.py

--- incomum/serializers/relatorioSerializer.py
+++ incomum/serializers/relatorioSerializer.py
@@ -3,14 +3,7 @@ from ..models.relatorio import Relatorio
 import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-class RelatorioSerializer(serializers.Serializer):
-    aco_codigo = serializers.IntegerField(required=False, allow_null=True)
-    fim_codigo = serializers.IntegerField(required=False, allow_null=True)
-    fim_data = serializers.DateField()
-    fim_markup = serializers.FloatField()
-    fim_tipo = serializers.CharField()
-    fim_valorinc = serializers.FloatField()
-    fim_valorincajustado = serializers.FloatField()
-    fim_valorliquido = serializers.FloatField()
-    tur_codigo = serializers.IntegerField()
-    tur_numerovenda = serializers.CharField()
+class RelatorioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Relatorio
+        fields = '__all__'
