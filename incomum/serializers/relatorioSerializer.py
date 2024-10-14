c08783323a085d4266d360f946892db40a19aa49
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-10-14 15:25:29 -0300

    Update relatorioSerializer.py

--- incomum/serializers/relatorioSerializer.py
+++ incomum/serializers/relatorioSerializer.py
@@ -4,10 +4,6 @@ import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 class RelatorioSerializer(serializers.ModelSerializer):
-    # Campo customizado para valor formatado
-    valor_formatado = serializers.FloatField(source='valor', read_only=True)
-
     class Meta:
         model = Relatorio
-        fields = '__all__'  # Ou pode listar os campos específicos
-
+        fields = '__all__'
