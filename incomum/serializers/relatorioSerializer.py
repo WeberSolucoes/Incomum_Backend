3f05d41aa87813c5c176540bbc34498bf24f4b95
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-10-14 15:14:11 -0300

    Update relatorioSerializer.py

--- incomum/serializers/relatorioSerializer.py
+++ incomum/serializers/relatorioSerializer.py
@@ -1,23 +1,11 @@
 from rest_framework import serializers
 from ..models.relatorio import Relatorio
-import locale
-
-# Configurando a localização para 'pt_BR.UTF-8' (Brasil)
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 class RelatorioSerializer(serializers.ModelSerializer):
     # Campo customizado para valor formatado
-    valor_formatado = serializers.SerializerMethodField()
+    valor_formatado = serializers.FloatField(source='valor', read_only=True)
 
     class Meta:
         model = Relatorio
-        fields = '__all__'  # Ou pode listar os campos específicos, incluindo 'valor_formatado'
-
-    # Método para formatar o valor usando a localização brasileira
-    def get_valor_formatado(self, obj):
-        # Supondo que o campo que você quer formatar é 'valor'
-        if obj.valor:
-            # Formata o valor como moeda brasileira (R$)
-            return locale.currency(obj.valor, grouping=True)
-        return None
+        fields = '__all__'  # Ou pode listar os campos específicos
 
