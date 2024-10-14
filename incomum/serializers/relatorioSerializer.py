82c2710ea8f54a7690da12e74dc4c551b0a180fe
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-10-14 15:03:44 -0300

    Update relatorioSerializer.py

--- incomum/serializers/relatorioSerializer.py
+++ incomum/serializers/relatorioSerializer.py
@@ -4,6 +4,11 @@ import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 class RelatorioSerializer(serializers.ModelSerializer):
+    valor_formatado = serializers.SerializerMethodField()
+
     class Meta:
         model = Relatorio
-        fields = '__all__'
+        fields = '__all__'  # Ou pode listar os campos específicos, incluindo 'valor_formatado'
+
+    def get_valor_formatado(self, obj):
+        return locale.currency(obj.valor, grouping=True)
