d4af56aff9cf5a8cd22966662ad49b589c49d458
Ygor <86171558+ygor84@users.noreply.github.com>
2024-10-18 18:26:05 -0300

    Update vendedor.py

--- incomum/models/vendedor.py
+++ incomum/models/vendedor.py
@@ -2,13 +2,26 @@ from django.db import models
 
 
 class Vendedor(models.Model):
-    ven_codigo = models.AutoField(primary_key=True)  # Assuming this field is auto-incrementing
-    ven_descricao = models.CharField(max_length=60, null=True, blank=True)
-    ban_codigo = models.IntegerField(null=True, blank=True)
-    ven_agencia = models.CharField(max_length=7, null=True, blank=True)
-    ven_contacorrente = models.CharField(max_length=15, null=True, blank=True)
-    ven_descricaoweb = models.CharField(max_length=60, null=True, blank=True)
-    ven_codigoimportacao = models.IntegerField(null=True, blank=True)
+    ven_codigo = models.AutoField(primary_key=True)
+    ven_descricao = models.CharField(max_length=60, blank=True, null=True)
+    ven_endereco = models.CharField(max_length=60, blank=True, null=True)
+    ven_numero = models.CharField(max_length=15, blank=True, null=True)
+    ven_bairro = models.CharField(max_length=30, blank=True, null=True)
+    ven_cep = models.IntegerField(blank=True, null=True)
+    cid_codigo = models.IntegerField(blank=True, null=True)
+    ven_fone = models.CharField(max_length=17, blank=True, null=True)
+    ven_celular = models.CharField(max_length=17, blank=True, null=True)
+    ven_email = models.CharField(max_length=50, blank=True, null=True)
+    ven_observacao = models.CharField(max_length=170, blank=True, null=True)
+    ban_codigo = models.IntegerField(blank=True, null=True)
+    ven_agencia = models.CharField(max_length=7, blank=True, null=True)
+    ven_contacorrente = models.CharField(max_length=15, blank=True, null=True)
+    ven_cpf = models.CharField(max_length=20, blank=True, null=True)
+    ven_descricaoauxiliar = models.CharField(max_length=60, blank=True, null=True)
+    ven_situacao = models.CharField(max_length=1, blank=True, null=True)
+    ven_descricaoweb = models.CharField(max_length=60, blank=True, null=True)
+    ven_codigoimportacao = models.IntegerField(blank=True, null=True)
+    sve_codigo = models.IntegerField(blank=True, null=True)
     class Meta:
         db_table = 'vendedor'
         verbose_name = 'Vendedor'
