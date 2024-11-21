aad09916d2dc865b4520b0e961e2d96ff935a2a3
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-11-21 17:00:02 -0300

    Create tipoRegimeViews.py

---
+++ incomum/views/tipoRegimeViews.py
@@ -0,0 +1,79 @@
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import AllowAny
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+from ..serializers.tipoRegimeSerializer import *
+from ..models import *
+from ..services import tipoRegimeService
+
+@swagger_auto_schema(
+        methods=['get'], 
+        responses={200: TipoRegimeSerializer},
+        tags=['Loja'])
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def find_by_id(request, id):
+    return tipoRegimeService.findById(id)
+
+@swagger_auto_schema(
+        methods=['post'],
+        request_body=TipoRegimeSerializer,
+        responses={201: TipoRegimeSerializer},
+        tags=['Loja'])
+@api_view(['POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def create(request):
+    return tipoRegimeService.create(request)
+
+@swagger_auto_schema(
+        methods=['get'],
+        responses={200: TipoRegimeSerializer(many=True)},
+        tags=['Loja'])
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def find_by_loja(request, id):
+    return tipoRegimeService.findByLoja(id)
+
+@swagger_auto_schema(
+        methods=['put'],
+        request_body=TipoRegimeSerializer,
+        responses={200: TipoRegimeSerializer},
+        tags=['Loja'])
+@api_view(['PUT'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def update(request, id):
+    return tipoRegimeService.update(request, id)
+
+@swagger_auto_schema(
+        methods=['delete'],
+        responses={204: openapi.Response(description="No Content")},
+        tags=['Loja'])
+@api_view(['DELETE'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def delete(request, id):
+    return tipoRegimeService.delete(id)
+
+@swagger_auto_schema(
+        methods=['get'],
+        responses={200: TipoRegimeSerializer(many=True)},
+        tags=['Loja'])
+@api_view(['GET'])
+
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
+def list_all(request):
+    return tipoRegimeService.list_all()
