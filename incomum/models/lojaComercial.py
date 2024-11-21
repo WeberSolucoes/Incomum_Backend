e9e74d69f8b21d39d1acd2aced467499e2e84f61
Ygor <yguygudagostin@gmail.com>
2024-11-21 11:39:23 -0300

    Revert "LOJA COMERCIAL"
    This reverts commit 459e878c3824a8c276e674194f269ee21229b375.

--- incomum/models/lojaComercial.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class LojaComercial(models.Model):
-    loj_codigo = models.AutoField(primary_key=True)
-    aco_codigo = models.IntegerField()
-
-    class Meta:
-        db_table = 'lojacomercial'
-        verbose_name_plural = "LojasComercial"
