d46f15d64cbbc1abcc4b24121ec94e3d6aa97baf
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-02 08:25:25 -0300

    Create parceiroContato.py

---
+++ incomum/models/parceiroContato.py
@@ -0,0 +1,18 @@
+
+from django.db import models
+from incomum.models.parceiro import Parceiro
+
+class ParceiroContato(models.Model):
+    pco_codigo = models.IntegerField(primary_key=True)
+    par_codigo = models.IntegerField()
+    pco_descricao = models.CharField(max_length=255,null=True, blank=True)
+    pco_observacao = models.CharField(max_length=255,null=True, blank=True)
+    pco_fone = models.CharField(max_length=255,null=True, blank=True)
+    pco_celular = models.CharField(max_length=255,null=True, blank=True)
+    pco_datacadastro = models.CharField(max_length=255,null=True, blank=True)
+    tco_codigo = models.IntegerField(null=True, blank=True)
+
+
+    class Meta:
+        db_table = 'parceirocontato'
+        #managed = False
