b2a79c4de1610c253e1207a496ac7b8e2408c3aa
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-05 08:29:55 -0300

    Create bancoViews.py

---
+++ incomum/views/bancoViews.py
@@ -0,0 +1,79 @@
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import AllowAny
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+from ..serializers.bancoSerializer import *
+from ..models import *
+from ..services import bancoService
+
+@swagger_auto_schema(
+        methods=['get'], 
+        responses={200: BancoSerializer},
+        tags=['Loja'])
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def find_by_id(request, id):
+    return bancoService.findById(id)
+
+@swagger_auto_schema(
+        methods=['post'],
+        request_body=BancoSerializer,
+        responses={201: BancoSerializer},
+        tags=['Loja'])
+@api_view(['POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def create(request):
+    return bancoService.create(request)
+
+@swagger_auto_schema(
+        methods=['get'],
+        responses={200: BancoSerializer(many=True)},
+        tags=['Loja'])
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def find_by_loja(request, id):
+    return bancoService.findByLoja(id)
+
+@swagger_auto_schema(
+        methods=['put'],
+        request_body=BancoSerializer,
+        responses={200: BancoSerializer},
+        tags=['Loja'])
+@api_view(['PUT'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def update(request, id):
+    return bancoService.update(request, id)
+
+@swagger_auto_schema(
+        methods=['delete'],
+        responses={204: openapi.Response(description="No Content")},
+        tags=['Loja'])
+@api_view(['DELETE'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def delete(request, id):
+    return bancoService.delete(id)
+
+@swagger_auto_schema(
+        methods=['get'],
+        responses={200: BancoSerializer(many=True)},
+        tags=['Loja'])
+@api_view(['GET'])
+
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
+def list_all(request):
+    return bancoService.list_all()
