2dd292d277db0e46f70a28cef4bfa704ac7bae9f
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-06 09:01:11 -0300

    Update relatorioViews.py

--- incomum/views/relatorioViews.py
+++ incomum/views/relatorioViews.py
@@ -81,7 +81,6 @@ def list_all_areas(request, unidade_id=None):
         methods=['get'],
         tags=['Relatorio'])
 @api_view(['GET'])
-@authentication_classes([JWTAuthentication])
 @permission_classes([AllowAny])
 def create_excel_byfilter(request):
     return relatorioService.create_excel_byfilter(request)
