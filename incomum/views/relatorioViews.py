76c43e12786d61f9636a892afe3305dec4ef965e
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-09 09:54:50 -0300

    Update relatorioViews.py

--- incomum/views/relatorioViews.py
+++ incomum/views/relatorioViews.py
@@ -81,6 +81,7 @@ def list_all_areas(request, unidade_id=None):
     methods=['get'],
     tags=['Relatorio'])
 @api_view(['GET'])
+@authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])  # Exige autenticação
 def create_excel_byfilter(request):
     print(f"Headers: {request.headers}")  # Log para verificar cabeçalhos
