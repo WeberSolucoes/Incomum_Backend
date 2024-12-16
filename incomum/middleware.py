53489979d06e55e342232dd384e86a9fe1eb0fa4
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-16 11:11:12 -0300

    Update middleware.py

--- incomum/middleware.py
+++ incomum/middleware.py
@@ -2,22 +2,22 @@
 from django.http import HttpResponseForbidden
 
 class RestrictIPMiddleware:
-    ALLOWED_IPS = ['3.21.123.210', '172.31.28.99']  # Adicione o IP público e privado
+    ALLOWED_IPS = ['3.21.123.210']  # IP público do seu frontend (IP elástico)
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Verificar o IP da requisição
         ip = request.META.get('REMOTE_ADDR')
+        # Se a requisição passar por um proxy (Cloudflare, por exemplo), use o X-Forwarded-For
         forwarded_ip = request.META.get('HTTP_X_FORWARDED_FOR')
         
-        # Caso esteja atrás de um proxy, considere o X-Forwarded-For
         if forwarded_ip:
             ip = forwarded_ip.split(',')[0]
 
         if ip not in self.ALLOWED_IPS:
             return HttpResponseForbidden("Access denied.")
         return self.get_response(request)
+
 
 
