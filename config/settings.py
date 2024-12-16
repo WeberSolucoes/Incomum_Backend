66e516d17cc884e0c34ac659c7ea5791f834384d
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-16 11:15:10 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'incomum.middleware.RestrictIPMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
