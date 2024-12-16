e0aa964cbce26ae69637cc5bb5fb4441e14552ae
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-16 09:17:35 -0300

    Create subgrupo.py

---
+++ incomum/models/subgrupo.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Subgrupo(models.Model):
+    sbc_codigo = models.AutoField(primary_key=True)
+    sbc_descricao = models.CharField(max_length=50, null=True, blank=True)
+    grc_codigo = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'subgrupo'
+        # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
