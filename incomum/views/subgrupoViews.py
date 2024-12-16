c0e2aff1a0bd994523c544714e73688ae8e47256
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-16 09:06:04 -0300

    Create subgrupoViews.py

---
+++ incomum/views/subgrupoViews.py
@@ -0,0 +1,79 @@
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import AllowAny
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+from ..serializers.subgrupoSerializer import *
+from ..models import *
+from ..services import subgrupoService
+
+@swagger_auto_schema(
+        methods=['get'], 
+        responses={200: SubgrupoSerializer},
+        tags=['Loja'])
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def find_by_id(request, id):
+    return subgrupoService.findById(id)
+
+@swagger_auto_schema(
+        methods=['post'],
+        request_body=SubgrupoSerializer,
+        responses={201: SubgrupoSerializer},
+        tags=['Loja'])
+@api_view(['POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def create(request):
+    return subgrupoService.create(request)
+
+@swagger_auto_schema(
+        methods=['get'],
+        responses={200: SubgrupoSerializer(many=True)},
+        tags=['Loja'])
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def find_by_loja(request, id):
+    return subgrupoService.findByLoja(id)
+
+@swagger_auto_schema(
+        methods=['put'],
+        request_body=SubgrupoSerializer,
+        responses={200: SubgrupoSerializer},
+        tags=['Loja'])
+@api_view(['PUT'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def update(request, id):
+    return subgrupoService.update(request, id)
+
+@swagger_auto_schema(
+        methods=['delete'],
+        responses={204: openapi.Response(description="No Content")},
+        tags=['Loja'])
+@api_view(['DELETE'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny]) 
+@permission_classes([IsAuthenticated])
+def delete(request, id):
+    return subgrupoService.delete(id)
+
+@swagger_auto_schema(
+        methods=['get'],
+        responses={200: SubgrupoSerializer(many=True)},
+        tags=['Loja'])
+@api_view(['GET'])
+
+@authentication_classes([JWTAuthentication])
+@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
+def list_all(request):
+    return subgrupoService.list_all()
