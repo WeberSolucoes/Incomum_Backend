f67a9a23a7e7ab5c4b4bd8825b91ed8318a09425
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-16 10:55:16 -0300

    Update middleware.py

--- incomum/middleware.py
+++ incomum/middleware.py
@@ -1,14 +1,19 @@
-# myapp/middleware.py
 from django.http import HttpResponseForbidden
 
 class RestrictIPMiddleware:
-    ALLOWED_IPS = ['3.21.123.210']  # Defina o IP permitido aqui
+    ALLOWED_IPS = ['3.21.123.210', '172.31.28.99']  # Adicione os IPs públicos e privados permitidos aqui
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         ip = request.META.get('REMOTE_ADDR')
+        # Se a requisição estiver atrás de um proxy, verifique o cabeçalho 'X-Forwarded-For'
+        forwarded_ip = request.META.get('HTTP_X_FORWARDED_FOR')
+        if forwarded_ip:
+            ip = forwarded_ip.split(',')[0]  # Pega o primeiro IP da lista no cabeçalho
+
         if ip not in self.ALLOWED_IPS:
             return HttpResponseForbidden("Access denied.")
         return self.get_response(request)
+
