0b7bc227797d79b7341240e68f76f172bf697c35
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-17 14:09:15 -0300

    Update cepViews.py

--- incomum/views/cepViews.py
+++ incomum/views/cepViews.py
@@ -15,7 +15,6 @@ from ..services import cepService
         tags=['Loja'])
 @api_view(['GET'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def find_by_id(request, id):
     return cepService.findById(id)
@@ -26,8 +25,7 @@ def find_by_id(request, id):
         responses={201: CepSerializer},
         tags=['Loja'])
 @api_view(['POST'])
-@authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
+@authentication_classes([JWTAuthentication]) 
 @permission_classes([IsAuthenticated])
 def create(request):
     return cepService.create(request)
@@ -37,8 +35,7 @@ def create(request):
         responses={200: CepSerializer(many=True)},
         tags=['Loja'])
 @api_view(['GET'])
-@authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
+@authentication_classes([JWTAuthentication]) 
 @permission_classes([IsAuthenticated])
 def find_by_loja(request, id):
     return cepService.findByLoja(id)
@@ -50,7 +47,6 @@ def find_by_loja(request, id):
         tags=['Loja'])
 @api_view(['PUT'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def update(request, id):
     return cepService.update(request, id)
@@ -61,7 +57,6 @@ def update(request, id):
         tags=['Loja'])
 @api_view(['DELETE'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def delete(request, id):
     return cepService.delete(id)
@@ -73,7 +68,6 @@ def delete(request, id):
 @api_view(['GET'])
 
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny])
 @permission_classes([IsAuthenticated])
 def list_all(request):
     return cepService.list_all()
