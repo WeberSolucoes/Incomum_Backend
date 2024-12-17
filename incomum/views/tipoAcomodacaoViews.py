9b18afaf13c1db3b416ab2532d8a8b83b9df3fa4
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-17 14:30:28 -0300

    Update tipoAcomodacaoViews.py

--- incomum/views/tipoAcomodacaoViews.py
+++ incomum/views/tipoAcomodacaoViews.py
@@ -15,7 +15,6 @@ from ..services import tipoAcomodacaoService
         tags=['Loja'])
 @api_view(['GET'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def find_by_id(request, id):
     return tipoAcomodacaoService.findById(id)
@@ -27,7 +26,6 @@ def find_by_id(request, id):
         tags=['Loja'])
 @api_view(['POST'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def create(request):
     return tipoAcomodacaoService.create(request)
@@ -38,7 +36,6 @@ def create(request):
         tags=['Loja'])
 @api_view(['GET'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def find_by_loja(request, id):
     return tipoAcomodacaoService.findByLoja(id)
@@ -50,7 +47,6 @@ def find_by_loja(request, id):
         tags=['Loja'])
 @api_view(['PUT'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def update(request, id):
     return tipoAcomodacaoService.update(request, id)
@@ -61,7 +57,6 @@ def update(request, id):
         tags=['Loja'])
 @api_view(['DELETE'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def delete(request, id):
     return tipoAcomodacaoService.delete(id)
@@ -73,7 +68,6 @@ def delete(request, id):
 @api_view(['GET'])
 
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny])
 @permission_classes([IsAuthenticated])
 def list_all(request):
     return tipoAcomodacaoService.list_all()
