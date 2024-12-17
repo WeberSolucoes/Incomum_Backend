4d10cc45e21b04720a24063645b5f6d313a5e9e1
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-17 14:16:31 -0300

    Update despesasViews.py

--- incomum/views/despesasViews.py
+++ incomum/views/despesasViews.py
@@ -15,7 +15,6 @@ from ..services import despesasService
         tags=['Loja'])
 @api_view(['GET'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def find_by_id(request, id):
     return despesasService.findById(id)
@@ -27,7 +26,6 @@ def find_by_id(request, id):
         tags=['Loja'])
 @api_view(['POST'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def create(request):
     return despesasService.create(request)
@@ -38,7 +36,6 @@ def create(request):
         tags=['Loja'])
 @api_view(['GET'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def find_by_loja(request, id):
     return despesasService.findByLoja(id)
@@ -50,7 +47,6 @@ def find_by_loja(request, id):
         tags=['Loja'])
 @api_view(['PUT'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def update(request, id):
     return despesasService.update(request, id)
@@ -61,7 +57,6 @@ def update(request, id):
         tags=['Loja'])
 @api_view(['DELETE'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny]) 
 @permission_classes([IsAuthenticated])
 def delete(request, id):
     return despesasService.delete(id)
@@ -73,7 +68,6 @@ def delete(request, id):
 @api_view(['GET'])
 
 @authentication_classes([JWTAuthentication])
-@permission_classes([AllowAny])
 @permission_classes([IsAuthenticated])
 def list_all(request):
     return despesasService.list_all()
