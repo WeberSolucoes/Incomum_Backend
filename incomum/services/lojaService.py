be539797cd6302780aa941fafaf606e0463d09ac
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-01-02 17:29:37 -0300

    Update lojaService.py

--- incomum/services/lojaService.py
+++ incomum/services/lojaService.py
@@ -41,6 +41,6 @@ def delete(id) -> Response:
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 def list_all() -> Response:
-    lojas = Loja.objects.all()
+    lojas = Loja.objects.all().order_by('loj_descricao')  # 'nome' é o campo a ser ordenado
     serializer = LojaSerializer(lojas, many=True)
     return Response(serializer.data)
