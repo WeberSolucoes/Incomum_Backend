dcc43f566664b1de25551b8419af254e1935bddc
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-01-06 10:37:08 -0300

    Update relatorioSerializer.py

--- incomum/serializers/relatorioSerializer.py
+++ incomum/serializers/relatorioSerializer.py
@@ -6,5 +6,5 @@ locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 class RelatorioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Relatorio
-        fields = ['tur_codigo', 'fim_data', 'fim_tipo', 'tur_numerovenda','fim_valorliquido','fim_markup','fim_valorinc','fim_valorincajustado','aco_descricao','ven_descricao','age_descricao']  # Exemplo de campos selecionados
+        fields = ['tur_codigo', 'fim_data', 'fim_tipo', 'tur_numerovenda','fim_valorliquido','fim_markup','fim_valorinc','fim_valorincajustado','aco_descricao','ven_descricao','age_descricao','fat_valorvendabruta']  # Exemplo de campos selecionados
 
