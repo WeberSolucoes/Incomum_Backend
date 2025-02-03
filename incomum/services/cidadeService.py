3279a59778f568f27ae9ac19b1555d8f2f5e7181
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-03 08:26:49 -0300

    Update cidadeService.py

--- incomum/services/cidadeService.py
+++ incomum/services/cidadeService.py
@@ -52,20 +52,25 @@ from django.db import connection
 
 def search_cidades(request):
     query = request.GET.get('q', '').strip()
-    
+    print(f"Consultando por: {query}")
+
     if len(query) >= 3:
         with connection.cursor() as cursor:
             sql_query = """
-                SELECT cid_codigo, cid_descricao, cid_estado, reg_codigo, cid_pais, cid_sigla, pai_codigo
+                SELECT cid_codigo, cid_descricao, cid_estado
                 FROM cidade
                 WHERE LOWER(TRIM(cid_descricao)) LIKE LOWER(%s)
             """
             parametro = f'%{query}%'
-            
+            print(f"Executando SQL: {sql_query} com parâmetro {parametro}")
+
             cursor.execute(sql_query, [parametro])
             cidades = cursor.fetchall()
-            
-            cidade_list = [{'label': cidade[1], 'value': cidade[0]} for cidade in cidades]
+            print(f"Resultados da consulta: {cidades}")
+
+            # Inclui o UF (cid_estado) no retorno
+            cidade_list = [{'label': cidade[1], 'value': cidade[0], 'uf': cidade[2]} for cidade in cidades]
+
             return Response(cidade_list, status=status.HTTP_200_OK)
-    else:
-        return Response([], status=status.HTTP_200_OK)
+    
+    return Response([], status=status.HTTP_200_OK)
