8df7fec14540c11b1187a179eb220d7ef4815b1c
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-06 14:25:45 -0300

    Update relatorioService.py

--- incomum/services/relatorioService.py
+++ incomum/services/relatorioService.py
@@ -589,6 +589,8 @@ def obter_dados_agencia(request):
     unidade = request.GET.get('unidade')
     area = request.GET.getlist('area[]')
 
+     print(f"Filtrando por unidade: {unidade}, área comercial: {area}") 
+
     # Consulta inicial
     queryset = Relatorio.objects.all()
 
