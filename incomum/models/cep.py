00fbf52dd15ae272a5f468a4cff28c882fc76bff
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-10 15:17:50 -0300

    Update cep.py

--- incomum/models/cep.py
+++ incomum/models/cep.py
@@ -4,9 +4,9 @@ from .loja import Loja
 class Cep(models.Model):
     cep_codigo = models.AutoField(primary_key=True)
     cep_logradouro = models.CharField(max_length=50, null=True, blank=True)
-    cep_bairro = models.CharField(null=True, blank=True)
+    cep_bairro = models.CharField(max_length=50,null=True, blank=True)
     cid_codigo = models.IntegerField(null=True, blank=True)
-    cep_numero = models.IntegerField(null=True, blank=True)
+    cep_numero = models.CharField(max_length=50,null=True, blank=True)
     tlo_codigo = models.IntegerField(null=True, blank=True)
 
     class Meta:
