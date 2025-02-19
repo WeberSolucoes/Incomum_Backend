3771d52231408fc64dcbeade692368ff40f61212
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 17:44:58 -0300

    Update aeroporto.py

--- incomum/models/aeroporto.py
+++ incomum/models/aeroporto.py
@@ -8,6 +8,14 @@ class Aeroporto(models.Model):
     aer_fone = models.CharField(max_length=50, null=True, blank=True)
     aer_email = models.CharField(max_length=50, null=True, blank=True)
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(Aeroporto, self).save(force_insert, force_update, *args, **kwargs)
+
     class Meta:
         db_table = 'aeroporto'
         # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
