62d962f6c39a0645c920c3cc767f82b2d96f724e
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 14:50:21 -0300

    Create duplicataService.py

---
+++ incomum/services/duplicataService.py
@@ -0,0 +1,46 @@
+from rest_framework.response import Response
+from rest_framework import status
+from ..models import *
+from ..serializers.duplicataSerializer import *
+
+def findById(id) -> Response:
+    try:
+        loja: Loja = Duplicata.objects.get(tdu_codigo = id)
+    except Duplicata.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = DuplicataSerializer(loja)
+    return Response(serializer.data)
+
+def create(request) -> Response:
+    serializer = DuplicataSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def update(request, id) -> Response:
+    try:
+        loja: Loja = Duplicata.objects.get(tdu_codigo = id)
+    except Duplicata.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = DuplicataSerializer(loja, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def delete(id) -> Response:
+    try:
+        loja: Loja = Duplicata.objects.get(tdu_codigo = id)
+    except Duplicata.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    loja.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+def list_all() -> Response:
+    lojas = Duplicata.objects.all()
+    serializer = DuplicataSerializer(lojas, many=True)
+    return Response(serializer.data)
