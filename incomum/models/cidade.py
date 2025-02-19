e7f2b96aee0af34bdc63ef1ffdf68f630b456aff
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 17:50:45 -0300

    Update cidade.py

--- incomum/models/cidade.py
+++ incomum/models/cidade.py
@@ -9,6 +9,14 @@ class Cidade(models.Model):
     cid_sigla = models.CharField(max_length=10, null=True, blank=True)
     pai_codigo = models.IntegerField(null=True, blank=True)
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(Cidade, self).save(force_insert, force_update, *args, **kwargs)
+
     class Meta:
         db_table = 'cidade'
         # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
