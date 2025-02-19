7ab5dad7c89aa94bf97fc218d259891e345f3b0c
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 17:51:42 -0300

    Update companhia.py

--- incomum/models/companhia.py
+++ incomum/models/companhia.py
@@ -9,6 +9,14 @@ class Companhia(models.Model):
     com_sigla = models.CharField(null=True, blank=True)
     par_codigo = models.IntegerField(null=True, blank=True)
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(Companhia, self).save(force_insert, force_update, *args, **kwargs)
+
     class Meta:
         db_table = 'companhia'
         # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
