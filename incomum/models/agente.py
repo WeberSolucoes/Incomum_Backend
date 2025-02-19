3f426e1b15b3b4625ee7dbf95447acae142eac33
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 17:47:57 -0300

    Update agente.py

--- incomum/models/agente.py
+++ incomum/models/agente.py
@@ -19,6 +19,14 @@ class Agente(models.Model):
     agt_agencia = models.IntegerField()
     agt_contacorrente = models.IntegerField()
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(Agente, self).save(force_insert, force_update, *args, **kwargs)
+
     class Meta:
         managed = False
         db_table = 'agente'
