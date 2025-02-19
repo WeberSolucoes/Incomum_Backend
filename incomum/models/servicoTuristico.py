183c5ef981a4bb02e0f2b5dfe40a623f9028cc59
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 18:06:40 -0300

    Update servicoTuristico.py

--- incomum/models/servicoTuristico.py
+++ incomum/models/servicoTuristico.py
@@ -6,6 +6,14 @@ class ServicoTuristico(models.Model):
     cid_codigo = models.IntegerField(null=True, blank=True)
     ser_livre = models.CharField(max_length=50, null=True, blank=True)
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(ServicoTuristico, self).save(force_insert, force_update, *args, **kwargs)
+
     class Meta:
         db_table = 'servico'
         # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
