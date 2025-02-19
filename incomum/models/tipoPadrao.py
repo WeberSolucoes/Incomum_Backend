63b9545b2e2b56ffc03cd7ffde9fdc033afd60fe
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 18:09:54 -0300

    Update tipoPadrao.py

--- incomum/models/tipoPadrao.py
+++ incomum/models/tipoPadrao.py
@@ -7,6 +7,14 @@ class TipoPadrao(models.Model):
     tpa_descricaoingles = models.CharField(null=True, blank=True)
     tpa_principal = models.CharField(null=True, blank=True)
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(TipoPadrao, self).save(force_insert, force_update, *args, **kwargs)
+
     class Meta:
         db_table = 'tipopadrao'
         # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
