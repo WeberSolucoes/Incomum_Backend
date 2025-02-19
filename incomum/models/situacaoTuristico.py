02cdb8588d808b9fb6f3713063a1efe3a31aa540
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 18:07:48 -0300

    Update situacaoTuristico.py

--- incomum/models/situacaoTuristico.py
+++ incomum/models/situacaoTuristico.py
@@ -4,6 +4,14 @@ class SituacaoTuristico(models.Model):
     stu_codigo = models.AutoField(primary_key=True)
     stu_descricao = models.CharField(max_length=50, null=True, blank=True)
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(SituacaoTuristico, self).save(force_insert, force_update, *args, **kwargs)
+
     class Meta:
         db_table = 'situacaoturistico'
         # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
