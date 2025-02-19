b3f4bfe5edb6b44a1b4a835842e173a873a70e4c
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 17:49:55 -0300

    Update centroCusto.py

--- incomum/models/centroCusto.py
+++ incomum/models/centroCusto.py
@@ -12,6 +12,14 @@ class CentroCusto(models.Model):
     cta_tipo = models.CharField(max_length=20, null=True, blank=True)
     cta_justificativa = models.CharField(max_length=20, null=True, blank=True)
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(CentroCusto, self).save(force_insert, force_update, *args, **kwargs)
+
     class Meta:
         db_table = 'conta'
         # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
