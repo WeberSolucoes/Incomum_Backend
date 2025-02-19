8edbaa54f4b5e4c6bda26900b937b37e0d7c301a
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 18:10:28 -0300

    Update tipoRegime.py

--- incomum/models/tipoRegime.py
+++ incomum/models/tipoRegime.py
@@ -6,6 +6,14 @@ class TipoRegime(models.Model):
     tre_descricaoportugues = models.CharField(null=True, blank=True)
     tre_descricaoingles = models.CharField(null=True, blank=True)
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(TipoRegime, self).save(force_insert, force_update, *args, **kwargs)
+
     class Meta:
         db_table = 'tiporegime'
         # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
