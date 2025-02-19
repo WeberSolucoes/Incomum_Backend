ce950b9dcf2a293a033a09a3a18851f5dfedcaaf
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 18:00:39 -0300

    Update parceiro.py

--- incomum/models/parceiro.py
+++ incomum/models/parceiro.py
@@ -34,6 +34,14 @@ class Parceiro(models.Model):
     par_whatsapp = models.CharField(max_length=255,null=True, blank=True)
     par_codigoimportacao = models.IntegerField(null=True, blank=True)
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(Parceiro, self).save(force_insert, force_update, *args, **kwargs)
+
 
     class Meta:
         db_table = 'parceiro'
