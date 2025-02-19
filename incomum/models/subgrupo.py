97b7528e22112369b9842ffb5aed6a254973aa08
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 15:04:44 -0300

    Update subgrupo.py

--- incomum/models/subgrupo.py
+++ incomum/models/subgrupo.py
@@ -6,5 +6,5 @@ class Subgrupo(models.Model):
     grc_codigo = models.IntegerField(null=True, blank=True)
 
     class Meta:
-        db_table = 'subgrupo'
+        db_table = 'subgrupoconta'
         # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
