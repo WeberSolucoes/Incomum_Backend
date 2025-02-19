d72531823d01aa4c41837bb833ea21e4fa66504a
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 17:50:23 -0300

    Update cep.py

--- incomum/models/cep.py
+++ incomum/models/cep.py
@@ -9,6 +9,14 @@ class Cep(models.Model):
     cep_numero = models.CharField(max_length=50,null=True, blank=True)
     tlo_codigo = models.IntegerField(null=True, blank=True)
 
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        for field in self._meta.fields:
+            if isinstance(field, models.CharField):
+                value = getattr(self, field.name)
+                if value:
+                    setattr(self, field.name, value.upper())
+        super(Cep, self).save(force_insert, force_update, *args, **kwargs)
+
     class Meta:
         db_table = 'cep'
         # managed = False  # Define como False se a tabela já existir no banco de dados e não quiser que o Django a gerencie
