5098a8b3e5f8725b32be04f563b8c86051301ecc
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-21 10:14:54 -0300

    Update despesasService.py

--- incomum/services/despesasService.py
+++ incomum/services/despesasService.py
@@ -41,6 +41,6 @@ def delete(id) -> Response:
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 def list_all() -> Response:
-    lojas = Despesas.objects.all()
+    lojas = Despesas.objects.all().order_by('mgr_descricao')
     serializer = DespesasSerializer(lojas, many=True)
     return Response(serializer.data)
