1eafe296fbfd054af8e30ad6a6a3160f476fe674
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-21 10:27:24 -0300

    Update tipoAcomodacaoService.py

--- incomum/services/tipoAcomodacaoService.py
+++ incomum/services/tipoAcomodacaoService.py
@@ -41,6 +41,6 @@ def delete(id) -> Response:
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 def list_all() -> Response:
-    lojas = TipoAcomodacao.objects.all()
+    lojas = TipoAcomodacao.objects.all().order_by('tac_descricao')
     serializer = TipoAcomodacaoSerializer(lojas, many=True)
     return Response(serializer.data)
