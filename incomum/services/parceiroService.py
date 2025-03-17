576c2d3aae35e0d9e02a852596d223dd3c2f570d
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-03-17 14:07:18 -0300

    Update parceiroService.py

--- incomum/services/parceiroService.py
+++ incomum/services/parceiroService.py
@@ -44,3 +44,27 @@ def list_all() -> Response:
     lojas = Parceiro.objects.all().order_by('par_descricao')
     serializer = ParceiroSerializer(lojas, many=True)
     return Response(serializer.data)
+
+
+from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Q
+from ..serializers.parceiroSerializer import ParceiroSerializer
+
+def search(request) -> Response:
+    search_term = request.query_params.get('search', '').strip()
+    limit = int(request.query_params.get('limit', 100))  # Limite de registros por requisição
+
+    # Filtra os fornecedores com base no termo de busca
+    if search_term:
+        fornecedores = Parceiro.objects.filter(
+            Q(par_descricao__icontains=search_term) |  # Busca pelo nome do fornecedor
+            Q(par_codigo__icontains=search_term)       # Busca pelo código do fornecedor
+        )[:limit]  # Aplica o limite
+    else:
+        fornecedores = Parceiro.objects.all()[:limit]  # Retorna os primeiros 100 registros se não houver busca
+
+    # Serializa os dados
+    serializer = ParceiroSerializer(fornecedores, many=True)
+    return Response(serializer.data)
+
